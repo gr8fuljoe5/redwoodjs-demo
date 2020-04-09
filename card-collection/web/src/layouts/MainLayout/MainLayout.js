dacae0907936f2eb7aa22c99f8781739d9663e14
Joe Kanakaraj <jkanakaraj@gmail.com>
2020-04-08 22:24:36 -0400

    adding layout

---
+++ card-collection/web/src/layouts/MainLayout/MainLayout.js
@@ -0,0 +1,9 @@
+const MainLayout = ({ children }) => {
+  return <>
+    <header>Card Collection</header>
+    <main>{children}</main>
+    <footer>footer</footer>
+  </>
+}
+
+export default MainLayout
