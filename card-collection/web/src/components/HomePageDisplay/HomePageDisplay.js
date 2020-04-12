e70398ac419ca583613bbbf82c45f8da33a8d9f2
Joe Kanakaraj <jkanakaraj@gmail.com>
2020-04-11 20:30:14 -0400

    adding filters and layout update

--- card-collection/web/src/components/HomePageDisplay/HomePageDisplay.js
+++ card-collection/web/src/components/HomePageDisplay/HomePageDisplay.js
@@ -1,18 +1,29 @@
-import Card from "src/components/Card/Card";
+import { useState } from 'react';
 import filter from 'lodash/filter';
 
 const HomePageDisplay = ({data}) => {
-  const filerData = (obj) => {
-    return filter(obj, (o) => {
-      if(o.sport == 'Baseball'){
-        return o;
+  const [filteredData, setFilteredData] = useState({});
+  const filterData = (value, column) => {
+    return filter(data, (o) => {
+      if(o[column] == value){
+        setFilteredData(o);
       }
     })
-  }
+  };
 
   return (
     <div>
+      <form>
+        <select onChange={(e) => {
+          console.log(e.target.value);
+          filterData(e.target.value, 'sport');
+        }}>
+          <option value="Baseball">Baseball</option>
+          <option value="Football">Football</option>
+        </select>
+      </form>
       <h1>Hello</h1>
+      {JSON.stringify(filteredData)}
     </div>
 
   )
--- card-collection/web/src/layouts/MainLayout/MainLayout.js
+++ card-collection/web/src/layouts/MainLayout/MainLayout.js
@@ -1,5 +1,16 @@
 const MainLayout = ({ children }) => {
-  return <>{children}</>
+  return <>
+    <header>
+      <h1>Paul's Baseball Card Collection</h1>
+      <nav>
+        <ul>
+          <li>Admin</li>
+        </ul>
+      </nav>
+    </header>
+    {children}
+
+    </>
 }
 
 export default MainLayout
