5614eb320367799d4dd64f24e99716683fd5e4ab
Joe Kanakaraj <jkanakaraj@gmail.com>
2020-05-19 14:53:23 -0400

    Adding donut chart and select box

--- covid-19/web/src/components/ByStateData/ByStateData.js
+++ covid-19/web/src/components/ByStateData/ByStateData.js
@@ -1,19 +1,24 @@
 import { Doughnut } from 'react-chartjs-2';
 
 const ByStateData = (obj) => {
-  const { death, recovered, total, positive, negative } = obj.data;
+  const { death, recovered, positive, negative } = obj.data;
   const data = {
-    labels: ['Positive Tests', 'Negative Tests', 'Death', 'Total', 'Recovered'],
+    labels: ['Positive Tests', 'Negative Tests', 'Death', 'Recovered'],
     datasets: [
       {
         backgroundColor: [
           'rgba(206, 102, 186, 0.3)',
           'rgba(193, 228, 37, 0.3)',
           'rgba(60, 91, 55, 0.3)',
-          'rgba(139, 255, 149, 0.3)',
           'rgba(109, 77, 178, 0.3)',
         ],
-        data: [positive, negative, death, total, recovered],
+        borderColor: [
+          'rgba(206, 102, 186, 1)',
+          'rgba(193, 228, 37, 1)',
+          'rgba(60, 91, 55, 1)',
+          'rgba(109, 77, 178, 1)',
+        ],
+        data: [positive, negative, death, recovered],
       },
     ],
   };
